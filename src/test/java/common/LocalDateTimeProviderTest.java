3758892308199b7799a0ab7be0ed7cb01adb0667
kelvin <kelvinwongyw@gmail.com>
2025-03-22 22:25:10 +1100

    added time provider test

---
+++ src/test/java/common/LocalDateTimeProviderTest.java
@@ -0,0 +1,24 @@
+package common;
+
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LocalDateTimeProviderTest {
+
+    @Test
+    public void testLocalDateTimeProvider() {
+        TimeProvider provider = new LocalDateTimeProvider();
+        LocalDateTime now = LocalDateTime.now();
+        ZonedDateTime zonedDateTime = now.atZone(ZoneId.systemDefault());
+        long currentTimeMillis = zonedDateTime.toInstant().toEpochMilli();
+        long nowFromProvider = provider.now();
+
+        assertTrue(currentTimeMillis - nowFromProvider < 1);
+    }
+
+}
